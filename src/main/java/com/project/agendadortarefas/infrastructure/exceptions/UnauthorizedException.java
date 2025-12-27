fb441539a5bfa910b8e2f5f3d26ae89c3dac03ef
Diego Furlan <furlanaugustodiego@gmail.com>
2025-12-27 02:46:57 -0300

    Adicionando exceptions personalizadas

---
+++ src/main/java/com/project/agendadortarefas/infrastructure/exceptions/UnauthorizedException.java
@@ -0,0 +1,16 @@
+package com.project.agendadortarefas.infrastructure.exceptions;
+
+
+import org.springframework.security.core.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+
+    public UnauthorizedException(String mensagem) {
+        super(mensagem);
+    }
+
+    public UnauthorizedException(String mensagem, Throwable throwable) {
+        super(mensagem, throwable);
+    }
+}
+
